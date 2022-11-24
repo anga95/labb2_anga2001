7cba6782cf55837f67e21595bdc3175428f5f747
anga95 <andreas.gabrielsson@live.se>
2022-11-24 12:13:53 +0100

    container-part

--- main.cpp
+++ main.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
+#include <vector>
 #include "shape.h"
 #include "circle.h"
 #include "cylinder.h"
 #include "rectangle.h"
 #include "parallelepiped.h"
 #include "rounded_rectangle.h"
+void f(std::vector<shape*> &vec){
+    for (auto &item  : vec) {
+        std::cout << item->getColor() << "\t" << item->getArea() << "\n";
+    }
 
+}
 int main() {
-
+    //f();
     circle circle("gulful", 10);
-    std::cout << "circle "<<circle.getArea() << " " << circle.getColor()<< "\n";
-
     cylinder cylinder("green", 10, 10);
-    std::cout << "cylinder " << cylinder.getArea() << " " << cylinder.getColor() << "\n";
+    rectangle rekt("blue", 10, 10);
+    parallelepiped par("grönsomfan", 10,10,10);
+    rounded_rectangle round("red",10,10,10);
+    std::vector<shape*> vec(5,nullptr);
+    vec[0] = &circle;
+    vec[1] = &cylinder;
+    vec[2] = &rekt;
+    vec[3] = &par;
+    vec[4] = &round;
 
-    rectangle rekt("blue", 5, 5);
-    std::cout <<"rekt "<< rekt.getArea()<< " " << rekt.getColor()<< "\n";
 
-    parallelepiped par("grönsomfan", 5,5,5);
-    std::cout << "parallellepiped " << par.getArea() << " " << par.getColor() << "\n";
 
-    rounded_rectangle round("red",5,10,2);
-    std::cout <<"round "<< round.getArea()<< " " << round.getColor() +"\n";
+
+
+
+
     return 0;
 }
+
+
+/*
+ * circle circle("gulful", 10);
+    std::cout << "circle \t\t\t\t"<<circle.getArea() << "\t\t" << circle.getColor()<< "\n";
+
+    cylinder cylinder("green", 10, 10);
+    std::cout << "cylinder \t\t\t" << cylinder.getArea() << "\t\t" << cylinder.getColor() << "\n";
+
+    rectangle rekt("blue", 5, 5);
+    std::cout <<"rectangle \t\t\t"<< rekt.getArea()<< "\t\t\t" << rekt.getColor()<< "\n";
+
+    parallelepiped par("grönsomfan", 5,5,5);
+    std::cout << "parallellepiped \t" << par.getArea() << "\t\t\t" << par.getColor() << "\n";
+
+    rounded_rectangle round("red",5,10,2);
+    std::cout <<"roundrectangle \t\t"<< round.getArea()<< "\t\t" << round.getColor() +"\n";
+ */
